d200ca678d269d712acaebc4ab4fb76ce92a9329
Ning Xiao <keoxiao@foxmail.com>
2020-09-22 11:50:29 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OTS3000NMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
